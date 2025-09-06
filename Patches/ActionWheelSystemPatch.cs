18b961c78c2b95a89b2d9c9b7a2e5ddc3f4651c3
zfolmt <mitchell.foltz@gmail.com>
2025-09-06 12:47:04 -0500

    Add category mode handling for quip wheel

--- Patches/ActionWheelSystemPatch.cs
+++ Patches/ActionWheelSystemPatch.cs
@@ -2,6 +2,8 @@
 using ProjectM.UI;
 using RetroCamera.Configuration;
 using RetroCamera.Utilities;
+using Stunlock.Localization;
+using System.Linq;
 using static RetroCamera.Configuration.QuipManager;
 using static RetroCamera.Utilities.Quips;
 using static RetroCamera.Systems.RetroCamera;
@@ -43,6 +45,69 @@ internal static class ActionWheelSystemPatch
     static DateTime _lastQuipSendTime = DateTime.MinValue;
     const float QUIP_COOLDOWN_SECONDS = 0.5f;
 
+    static bool _inCategoryMode = true;
+    static string _activeCategory = string.Empty;
+    static readonly LocalizationKey _backKey = LocalizationManager.GetLocalizationKey("Back");
+
+    static void PopulateCategories()
+    {
+        var categories = CommandCategories.Keys.ToList();
+        var dataList = ActionWheelSystem._SocialWheelDataList;
+        var shortcuts = ActionWheelSystem._SocialWheelShortcutList;
+
+        for (int i = 0; i < dataList.Count; i++)
+        {
+            ActionWheelData data = dataList[i];
+            if (i < categories.Count)
+            {
+                data.Name = LocalizationManager.GetLocalizationKey(categories[i]);
+                data.CategoryName = string.Empty;
+                shortcuts[i]?.gameObject?.SetActive(true);
+            }
+            else
+            {
+                data.Name = string.Empty;
+                data.CategoryName = string.Empty;
+                shortcuts[i]?.gameObject?.SetActive(false);
+            }
+
+            dataList[i] = data;
+        }
+    }
+
+    static void PopulateQuips(string category)
+    {
+        var quips = GetQuipsForCategory(category);
+        var dataList = ActionWheelSystem._SocialWheelDataList;
+        var shortcuts = ActionWheelSystem._SocialWheelShortcutList;
+
+        ActionWheelData backData = dataList[0];
+        backData.Name = _backKey;
+        backData.CategoryName = string.Empty;
+        dataList[0] = backData;
+        shortcuts[0]?.gameObject?.SetActive(true);
+
+        for (int i = 1; i < dataList.Count; i++)
+        {
+            ActionWheelData data = dataList[i];
+            int quipIndex = i - 1;
+            if (quipIndex < quips.Count)
+            {
+                data.Name = quips[quipIndex].NameKey;
+                data.CategoryName = category;
+                shortcuts[i]?.gameObject?.SetActive(true);
+            }
+            else
+            {
+                data.Name = string.Empty;
+                data.CategoryName = string.Empty;
+                shortcuts[i]?.gameObject?.SetActive(false);
+            }
+
+            dataList[i] = data;
+        }
+    }
+
     [HarmonyPatch(typeof(ActionWheelSystem), nameof(ActionWheelSystem.SendQuipChatMessage))]
     [HarmonyPrefix]
     static bool SendQuipChatMessagePrefix(byte index)
@@ -62,6 +127,39 @@ internal static class ActionWheelSystemPatch
 
         _lastQuipSendTime = now;
 
+        if (SocialWheelActive)
+        {
+            if (_inCategoryMode)
+            {
+                var categories = CommandCategories.Keys.ToList();
+                if (index < categories.Count)
+                {
+                    _activeCategory = categories[index];
+                    _inCategoryMode = false;
+                    PopulateQuips(_activeCategory);
+                }
+
+                return false;
+            }
+
+            if (index == 0)
+            {
+                _inCategoryMode = true;
+                _activeCategory = string.Empty;
+                PopulateCategories();
+                return false;
+            }
+
+            var quips = GetQuipsForCategory(_activeCategory);
+            int quipIndex = index - 1;
+            if (quipIndex >= 0 && quipIndex < quips.Count)
+            {
+                SendCommandQuip(quips[quipIndex]);
+            }
+
+            return false;
+        }
+
         if (CommandQuips.TryGetValue(index, out CommandQuip commandQuip))
         {
             SendCommandQuip(commandQuip);
@@ -77,6 +175,9 @@ internal static class ActionWheelSystemPatch
     {
         if (SocialWheelActive)
         {
+            _inCategoryMode = true;
+            _activeCategory = string.Empty;
+            PopulateCategories();
             return false;
         }
         else if (!_wheelOpened.Equals(DateTime.MinValue))
