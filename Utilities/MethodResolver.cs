5a212be560beff2c8d900855765c05a2caee69db
zfolmt <mitchell.foltz@gmail.com>
2025-09-07 16:25:07 -0500

    Expand IL2CPP method resolver

--- Utilities/MethodResolver.cs
+++ Utilities/MethodResolver.cs
@@ -2,6 +2,7 @@ using Iced.Intel;
 using Il2CppInterop.Common;
 using Il2CppInterop.Runtime.Runtime;
 using Il2CppInterop.Runtime.Runtime.VersionSpecific.MethodInfo;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace RetroCamera.Utilities;
@@ -21,33 +22,101 @@ internal static class MethodResolver
 
     static unsafe IntPtr ResolveMethodPointer(IntPtr methodPointer)
     {
-        var stream = new UnmanagedMemoryStream((byte*)methodPointer, 256, 256, FileAccess.Read);
-        var codeReader = new StreamCodeReader(stream);
+        var originalPointer = methodPointer;
+        var patterns = new List<string>();
 
-        var decoder = Decoder.Create(IntPtr.Size == 8 ? 64 : 32, codeReader);
-        decoder.IP = (ulong)methodPointer.ToInt64();
-
-        Instruction instr = default;
-        while (instr.Mnemonic != Mnemonic.Int3)
+        for (var depth = 0; depth < 6; depth++)
         {
-            decoder.Decode(out instr);
+            var stream = new UnmanagedMemoryStream((byte*)methodPointer, 256, 256, FileAccess.Read);
+            var decoder = Decoder.Create(IntPtr.Size == 8 ? 64 : 32, new StreamCodeReader(stream));
+            decoder.IP = (ulong)methodPointer.ToInt64();
 
-            if (instr.Mnemonic != Mnemonic.Jmp && instr.Mnemonic != Mnemonic.Add)
-            {
-                return methodPointer;
-            }
+            ulong movRaxImm = 0;
+            bool resolved = false;
 
-            if (instr.Mnemonic == Mnemonic.Add)
+            for (var i = 0; i < 16; i++)
             {
-                if (instr.Immediate32 != 0x10)
+                decoder.Decode(out var instr);
+
+                if (instr.Mnemonic == Mnemonic.Int3)
+                    break;
+
+                if (instr.Mnemonic == Mnemonic.Add)
                 {
-                    return methodPointer;
+                    if (instr.Op0Register == Register.RCX && instr.Immediate32 == 0x10)
+                        continue;
+
+                    break;
                 }
+
+                if (instr.Mnemonic == Mnemonic.Mov &&
+                    instr.Op0Register == Register.RAX &&
+                    instr.Op1Kind == OpKind.Immediate64)
+                {
+                    movRaxImm = instr.Immediate64;
+                    continue;
+                }
+
+                if (instr.Mnemonic == Mnemonic.Jmp)
+                {
+                    if (instr.Op0Kind == OpKind.Register && instr.Op0Register == Register.RAX && movRaxImm != 0)
+                    {
+                        methodPointer = (IntPtr)(long)movRaxImm;
+                        patterns.Add("mov rax, imm64; jmp rax");
+                        resolved = true;
+                        break;
+                    }
+
+                    if (instr.Op0Kind == OpKind.NearBranch16 ||
+                        instr.Op0Kind == OpKind.NearBranch32 ||
+                        instr.Op0Kind == OpKind.NearBranch64 ||
+                        instr.Op0Kind == OpKind.FarBranch16 ||
+                        instr.Op0Kind == OpKind.FarBranch32)
+                    {
+                        methodPointer = new IntPtr((long)ExtractTargetAddress(instr));
+                        patterns.Add("jmp imm");
+                        resolved = true;
+                        break;
+                    }
+
+                    if (instr.IsIPRelativeMemoryOperand)
+                    {
+                        methodPointer = (IntPtr)(long)(*(ulong*)instr.IPRelativeMemoryAddress);
+                        patterns.Add("jmp [rip+disp32]");
+                        resolved = true;
+                        break;
+                    }
+
+                    break;
+                }
+
+                if (instr.Mnemonic == Mnemonic.Push &&
+                    (instr.Op0Kind == OpKind.Immediate64 || instr.Op0Kind == OpKind.Immediate32))
+                {
+                    decoder.Decode(out var next);
+
+                    if (next.Mnemonic == Mnemonic.Ret)
+                    {
+                        ulong imm = instr.Op0Kind == OpKind.Immediate64 ? instr.Immediate64 : instr.Immediate32;
+                        methodPointer = (IntPtr)(long)imm;
+                        patterns.Add("push imm64; ret");
+                        resolved = true;
+                    }
+
+                    break;
+                }
+
+                break;
             }
 
-            if (instr.Mnemonic == Mnemonic.Jmp)
-                return new IntPtr((long)ExtractTargetAddress(instr));
+            if (!resolved)
+                break;
         }
+
+        if (patterns.Count > 0)
+            Core.Log.LogInfo($"[MethodResolver] Resolved {originalPointer} -> {methodPointer} via {string.Join(" -> ", patterns)}");
+        else
+            Core.Log.LogInfo($"[MethodResolver] No resolution for {methodPointer}");
 
         return methodPointer;
     }
